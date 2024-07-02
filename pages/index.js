48376851b98035d35295bb09071052895ddc78d6
David Solis <doze.inept.0n@icloud.com>
2024-07-01 18:36:27 -0700

    updated resume

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="container">
+	<div className="container">
 	  <Head>
 		<title>David Solis</title>
 		<meta name="description" content="David Solis' personal portfolio" />
@@ -17,75 +17,97 @@ export default function Home() {
 	  </header>
 	
 	  <nav className="neumorphismCard nav">
-		<a href="#skills">Skills</a>
 		<a href="#experience">Experience</a>
+		<a href="#skills">Skills</a>
 		<a href="#education">Education</a>
-		<a href="#mentorship">Mentorship</a>
 	  </nav>
 	
 	  <section id="about-me" className="neumorphismCard">
 		<h2>About Me</h2>
-		<p>Passionate educator with a background in technology, dedicated to inspiring students through engaging, digitally enhanced learning experiences using interactive tools.</p>
-	  </section>
-	
-	  <section id="skills" className="neumorphismCard">
-		<h2>Skills</h2>
-		<h3>Technical Skills</h3>
-		<ul className="skills-list">
-		  <li>Troubleshooting hardware and software issues for Apple desktop and mobile computers</li>
-		  <li>Training and development for technical support teams and sales staff</li>
-		  <li>Remote client technical support and training</li>
-		  <li>Proficient in mobile app and website development</li>
-		</ul>
-		<h3>Soft Skills</h3>
-		<ul className="skills-list">
-		  <li>Strong communication, customer service skills, and empathy in educational settings</li>
-		  <li>Ability to work under pressure, meet deadlines, and coordinate teams</li>
-		  <li>Skilled in delegating tasks and delivering quality service</li>
-		  <li>Detail-oriented with a strong commitment to excellence</li>
-		</ul>
+		<p>Self taught developer with a background in sales, education & technical support.</p>
 	  </section>
 	
 	  <section id="experience" className="neumorphismCard">
 		<h2>Experience</h2>
+		
+		<div className="job">
+		  <h3>Site Instructor at Girls Who Code</h3>
+		  <span>Summer 2019 - 2021</span>
+		  <ul>
+			<li>Led an in-person classroom of over twenty aspiring computer scientists, covering Python, HTML, CSS, Github, JavaScript & Arduino C over seven weeks.</li>
+			<li>Developed core skills such as Agile development, debugging & the importance of collaboration within a software team while offering college preparedness.</li>
+		  </ul>
+		</div>
+		
 		<div className="job">
 		  <h3>Software Developer at Peaking Software</h3>
-		  <span>2016 - Present</span>
+		  <span>2016 - 2021</span>
 		  <ul>
-			<li>Developed software solutions encompassing databases, servers, websites, and native Apple applications customized to meet individual client needs</li>
-			<li>Implemented project roadmaps when available and collaborated with clients and stakeholders to design project specifications as required</li>
+			<li>Learned to build iPhone apps using Swift Playgrounds. Embraced Apple’s latest framework SwiftUI for building my latest iPhone & iPad apps.</li>
+			<li>Developed databases, servers, websites and iPhone apps for clients.</li>
 		  </ul>
 		</div>
+		
 		<div className="job">
-		  <h3>Support Trainer at Bloomberg LP</h3>
+		  <h3>Remote Client Support at Bloomberg LP</h3>
 		  <span>2015 - 2016</span>
 		  <ul>
-			<li>Provided training & technical support for PC and mobile software, coordinated field service visits, and performed troubleshooting</li>
-			<li>Led the initiative to create a Wikipedia for the San Francisco technical support team, ensuring clear, concise, and accurate documentation of systems and procedures</li>
+			<li>Offered training & technical support for PC and mobile software, coordinated field service visits, and performed troubleshooting.</li>
+			<li>Steered the initiative to create a wiki for the San Francisco technical support team, ensuring clear, concise, and accurate documentation of systems and procedures.</li>
 		  </ul>
 		</div>
+		
 		<div className="job">
-		  <h3>Training Lead at Apple</h3>
-		  <span>2011 - 2015</span>
+		  <h3>Genius at Apple</h3>
+		  <span>2013 - 2015</span>
 		  <ul>
-			<li>Developed & facilitated training ranging from employee on-boarding, ongoing sales training, technical training, & train the trainer workshops</li>
-			<li>Increased product & technical knowledge at the market level with my employee mentorship initiative, which earned the market Q2 2013 Most Improved Net Promoter Score</li>
+			<li>Supplied leadership with support including providing overrides for the Redzone, relief for Leader on the Floor for breaks and conducted One to One trainings.</li>
+			<li>Provided exceptional customer service and offered complete solutions to customers, as reflected by my high NPS score as a technician in a Training Store.</li>
 		  </ul>
 		</div>
+		
+		<div className="job">
+		  <h3>In Store Guest Trainer at Apple</h3>
+		  <span>2013</span>
+		  <ul>
+			<li>Developed & facilitated training for employee on-boarding, ongoing sales training, technical training, & train the trainer workshops as In Store Guest Trainer.</li>
+			<li>Increased product & technical knowledge at the market level with my employee mentorship initiative, which earned the market Q2 2013 Most Improved Net Promoter Score.</li>
+		  </ul>
+		</div>
+		
+		<div className="job">
+		  <h3>Specialist at Apple</h3>
+		  <span>2011 - 2013</span>
+		  <ul>
+			<li>Led in store workshop team which earned the store Net Promoter Score of 98 for three quarters.</li>
+			<li>Shared extensive iPhone activation knowledge with the team, proving myself a resource for not only other specialists but also leadership.</li>
+		  </ul>
+		</div>
+	  </section>
+	  
+	  <section id="skills" className="neumorphismCard">
+		<h2>Skills</h2>		
+		<h3>Soft Skills</h3>
+		<ul className="skills-list">
+		  <li>Strong communication, customer service skills, and empathy in educational settings.</li>
+		  <li>Ability to work under pressure, meet deadlines, and coordinate teams.</li>
+		  <li>Skilled in delegating tasks and delivering quality service.</li>
+		  <li>Detail-oriented with a strong commitment to excellence.</li>
+		</ul>
+		
+		<h3>Technical Skills</h3>
+		<ul className="skills-list">
+		  <li>Troubleshooting hardware and software issues for Apple desktop and mobile computers.</li>
+		  <li>Training and development for technical support teams and sales staff.</li>
+		  <li>Remote client technical support and training.</li>
+		  <li>Proficient in mobile app and website development.</li>
+		</ul>
 	  </section>
 	
 	  <section id="education" className="neumorphismCard">
 		<h2>Education</h2>
 		<p>University of California, Riverside – Bachelors of Arts in History</p>
-	  </section>
-	
-	  <section id="mentorship" className="neumorphismCard">
-		<h2>Mentorship</h2>
-		<p>Site Instructor at Girls Who Code • Summer 2019 - 2021</p>
-		<ul>
-		  <li>Led an in-person classroom of over twenty aspiring computer scientists, covering Python, HTML, CSS, Github, JavaScript & Arduino C over seven weeks</li>
-		  <li>Summer immersion program develops core skills such as Agile development, debugging & the importance of collaboration within a software team while offering college preparedness</li>
-		</ul>
+		<p>Adult & Pediatric First Aid/CPR/AED</p>
 	  </section>
 	
 	  <footer className="neumorphismCard footer">
