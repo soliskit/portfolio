844c14150a3119ace147da0fbda54cdac7f8432b
David Solis <doze.inept.0n@icloud.com>
2024-10-24 12:12:11 -0700

    update with customer success copy

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
 	
 	  <section id="about-me" className="neumorphismCard">
 		<h2>About Me</h2>
-		<p>Self taught developer with a background in sales, education & technical support.</p>
+		<p>Customer success professional with expertise in sales, education, and technical support, delivering exceptional service, building relationships, and driving product adoption.</p>
 	  </section>
 	
 	  <section id="experience" className="neumorphismCard">
@@ -34,8 +34,8 @@ export default function Home() {
 		  <h3>Site Instructor at Girls Who Code</h3>
 		  <span>Summer 2019 - 2021</span>
 		  <ul>
-			<li>Led an in-person classroom of over twenty aspiring computer scientists, covering Python, HTML, CSS, GitHub, JavaScript & Arduino C over seven weeks.</li>
-			<li>Developed core skills such as Agile development, debugging & the importance of collaboration within a software team while offering college preparedness.</li>
+			<li>Guided over twenty students in Python, HTML, CSS, and JavaScript, fostering teamwork and agile development skills, helping students develop problem-solving abilities.</li>
+			<li>Emphasized debugging and teamwork while preparing students for higher education in computer science.</li>
 		  </ul>
 		</div>
 		
@@ -43,26 +43,27 @@ export default function Home() {
 		  <h3>Software Developer at Peaking Software</h3>
 		  <span>2016 - 2021</span>
 		  <ul>
-			<li>Learned to build iPhone apps using Swift Playgrounds. Embraced Apple’s latest framework SwiftUI for building my latest iPhone & iPad apps.</li>
-			<li>Developed databases, servers, websites and iPhone apps for clients.</li>
+			<li>Developed iPhone and iPad apps using Swift Playgrounds and SwiftUI, improving client workflows and driving satisfaction with tailored solutions.</li>
+			<li>Designed and maintained databases, servers, and websites tailored to customer needs, ensuring seamless product integration and functionality.</li>
+			<li>Collaborated closely with clients to identify needs, delivering tailored technical solutions that significantly improved user experience and satisfaction, directly contributing to product adoption and long-term retention.</li>
 		  </ul>
 		</div>
 		
 		<div className="job">
-		  <h3>Remote Client Support at Bloomberg LP</h3>
+		  <h3>Client Support at Bloomberg LP</h3>
 		  <span>2015 - 2016</span>
 		  <ul>
-			<li>Offered training & technical support for PC and mobile software, coordinated field service visits, and performed troubleshooting.</li>
-			<li>Steered the initiative to create a wiki for the San Francisco technical support team, ensuring clear, concise, and accurate documentation of systems and procedures.</li>
+			<li>Provided training and remote client support for PC and mobile software, handling troubleshooting inquiries and ensuring swift issue resolution.</li>
+			<li>Drove the development of a technical support knowledge base, enhancing team efficiency and offering clients self-service resources that improved resolution times.</li>
 		  </ul>
 		</div>
 		
 		<div className="job">
-		  <h3>Genius at Apple</h3>
+		  <h3>Genius Technician at Apple</h3>
 		  <span>2013 - 2015</span>
 		  <ul>
-			<li>Supplied leadership with support including providing overrides for the Redzone, relief for Leader on the Floor for breaks and conducted One to One trainings.</li>
-			<li>Provided exceptional customer service and offered complete solutions to customers, as reflected by my high NPS score as a technician in a Training Store.</li>
+			<li>Resolved complex hardware and software issues with consistently high customer satisfaction. Contributed to an elevated NPS in a fast-paced, high-volume environment, driving loyalty and retention.</li>
+			<li>Provided personalized One to One customer trainings, empowering users to fully leverage their Apple products, increasing product adoption, customer retention, and long-term loyalty. These tailored sessions consistently resulted in positive feedback and repeat customer engagement.</li>
 		  </ul>
 		</div>
 		
@@ -70,17 +71,17 @@ export default function Home() {
 		  <h3>In Store Guest Trainer at Apple</h3>
 		  <span>2013</span>
 		  <ul>
-			<li>Developed & facilitated training for employee on-boarding, ongoing sales training, technical training, & train the trainer workshops as In Store Guest Trainer.</li>
-			<li>Increased product & technical knowledge at the market level with my employee mentorship initiative, which earned the market Q2 2013 Most Improved Net Promoter Score.</li>
+			<li>Trained new hires and staff on product knowledge, troubleshooting, and customer service, improving team performance and driving customer success.</li>
+			<li>Developed and introduced mentorship programs that directly contributed to a significant increase in customer satisfaction, earning the Market Q2 Most Improved Net Promoter Score.</li>
 		  </ul>
 		</div>
 		
 		<div className="job">
-		  <h3>Specialist at Apple</h3>
+		  <h3>Sales Specialist at Apple</h3>
 		  <span>2011 - 2013</span>
 		  <ul>
-			<li>Led in store workshop team which earned the store Net Promoter Score of 98 for three quarters.</li>
-			<li>Participated in overnight shifts as part of the Visual Merchandising Team, preparing products and visuals, ensuring their presentation adhered to exact measurements based on guidelines.</li>
+			<li>Spearheaded in-store workshops, directly contributing to an exceptional Net Promoter Score of 98 for three consecutive quarters by enhancing customer education and satisfaction through tailored product demonstrations.</li>
+			<li>Collaborated with the Visual Merchandising Team during overnight shifts, ensuring products and displays were meticulously prepared to align with company guidelines, ultimately enhancing the customer experience through flawless presentation.</li>
 		  </ul>
 		</div>
 	  </section>
@@ -89,18 +90,18 @@ export default function Home() {
 		<h2>Skills</h2>		
 		<h3>Soft Skills</h3>
 		<ul className="skills-list">
-		  <li>Strong communication, customer service skills, and empathy in educational settings.</li>
-		  <li>Ability to work under pressure, meet deadlines, and coordinate teams.</li>
-		  <li>Skilled in delegating tasks and delivering quality service.</li>
-		  <li>Detail-oriented with a strong commitment to excellence.</li>
+		  <li>Exceptional communication and customer service skills, fostering strong relationships and delivering empathetic, customer-first support that drives customer success and satisfaction.</li>
+		  <li>Proven ability to work effectively under pressure, manage tight deadlines, and coordinate cross-functional teams to meet customer needs.</li>
+		  <li>Strong delegation and organizational skills, ensuring consistent service delivery with a customer-centric approach.</li>
+		  <li>Meticulous attention to detail and a deep commitment to customer success and satisfaction.</li>
 		</ul>
 		
 		<h3>Technical Skills</h3>
 		<ul className="skills-list">
-		  <li>Troubleshooting hardware and software issues for Apple desktop and mobile computers.</li>
-		  <li>Training and development for technical support teams and sales staff.</li>
-		  <li>Remote client technical support and training.</li>
-		  <li>Proficient in mobile app and website development.</li>
+		  <li>Expertise in troubleshooting hardware and software issues for Apple desktop and mobile devices, swiftly resolving technical problems to enhance the customer experience.</li>
+		  <li>Proven ability to train and develop teams, empowering technical support and sales staff to deliver superior customer service and drive customer success by effectively addressing customer needs.</li>
+		  <li>Experienced in providing remote technical support and training, ensuring customers can efficiently use products and services.</li>
+		  <li>Proficient in mobile app and website development, enabling a comprehensive understanding of technical issues from a customer’s perspective.</li>
 		</ul>
 	  </section>
 	
