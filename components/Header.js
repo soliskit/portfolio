86e62599b49458df4d70bb737399ef37ddbaa3a6
soliskit <doze.inept.0n@icloud.com>
2026-04-01 14:43:50 -0700

    update city to Los Angeles

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,18 @@
-import styles from './Header.module.css';
+import styles from './Header.module.css'
 
 export default function Header() {
   return (
-	<header className={styles.header}>
-	  <h1>David Solis</h1>
-	  <img src="/images/maya.png" alt="David Solis" className={styles.profilePhoto}/>
-	  <p>Carson, CA</p>
-	  <p>Email: <a href="mailto:hello@davidsolis.me">hello@davidsolis.me</a></p>
-	</header>
-  );
+    <header className={styles.header}>
+      <h1>David Solis</h1>
+      <img
+        src="/images/maya.png"
+        alt="David Solis"
+        className={styles.profilePhoto}
+      />
+      <p>Los Angeles, CA</p>
+      <p>
+        Email: <a href="mailto:hello@davidsolis.me">hello@davidsolis.me</a>
+      </p>
+    </header>
+  )
 }
