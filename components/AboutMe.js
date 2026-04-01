5ef85895abdf54dd663936278393ceaca8581fc3
soliskit <doze.inept.0n@icloud.com>
2026-04-01 14:40:31 -0700

    update about me

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,10 +1,13 @@
-import styles from './Card.module.css';
+import styles from './Card.module.css'
 
 export default function AboutMe() {
   return (
-	<section id="about-me" className={styles.card}>
-	  <h2>About Me</h2>
-	  <p>Customer success professional with expertise in sales, education, and technical support, delivering exceptional service, building relationships, and driving product adoption.</p>
-	</section>
-  );
+    <section id="about-me" className={styles.card}>
+      <h2>About Me</h2>
+      <p>
+        Deliver empathetic, customer first service in all of my sales,
+        educational and technical roles.
+      </p>
+    </section>
+  )
 }
--- components/Education.js
+++ components/Education.js
@@ -1,11 +1,10 @@
-import styles from './Card.module.css';
+import styles from './Card.module.css'
 
 export default function Education() {
   return (
-	<section id="education" className={styles.card}>
-	  <h2>Education</h2>
-	  <p>University of California, Riverside – Bachelors of Arts in History</p>
-	  <p>Adult & Pediatric First Aid/CPR/AED</p>
-	</section>
-  );
+    <section id="education" className={styles.card}>
+      <h2>Education</h2>
+      <p>University of California, Riverside – Bachelors of Arts in History</p>
+    </section>
+  )
 }
