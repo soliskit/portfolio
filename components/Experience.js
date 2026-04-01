24775d0bded6d5633b1c4be623ee21f24cd84852
soliskit <doze.inept.0n@icloud.com>
2026-04-01 14:33:05 -0700

    update resume

--- components/Experience.js
+++ components/Experience.js
@@ -1,64 +1,139 @@
-import styles from './Card.module.css';
+import styles from './Card.module.css'
 
 export default function Experience() {
   return (
-	<section id="experience" className={styles.card}>
-	  <h2>Experience</h2>
+    <section id="experience" className={styles.card}>
+      <h2>Experience</h2>
 
-	  <div className="job">
-		<h3>Site Instructor at Girls Who Code</h3>
-		<span>Summer 2019 - 2021</span>
-		<ul>
-		  <li>Guided over twenty students in Python, HTML, CSS, and JavaScript, fostering teamwork and agile development skills, helping students develop problem-solving abilities.</li>
-		  <li>Emphasized debugging and teamwork while preparing students for higher education in computer science.</li>
-		</ul>
-	  </div>
+      <div className="job">
+        <h3>Enagic</h3>
+        <span>2014 - Present</span>
+        <ul>
+          <li>
+            Monitored and recorded reliability issues reported by end users,
+            escalating them to management for review and resolution to uphold
+            product and service quality.
+          </li>
+          <li>
+            Employed SAP, a custom Customer Relationship Management (CRM)
+            system, and Zendesk to manage customer and distributor accounts,
+            process support requests, and ensure prompt issue resolution.
+          </li>
+          <li>
+            Developed and maintained Excel-based sales and performance reports
+            for accounting, operations, and sales teams, ensuring data accuracy
+            and consistency across departments.
+          </li>
+          <li>
+            Collaborated with cross-functional teams to verify distributor
+            information, review commission reports, and support account
+            reconciliation processes.
+          </li>
+          <li>
+            Provided responsive and high-quality support via email, phone, and
+            live chat, fostering trust and strong relationships with both
+            customers and distributors.
+          </li>
+        </ul>
+      </div>
 
-	  <div className="job">
-		<h3>Software Developer at Peaking Software</h3>
-		<span>2016 - 2021</span>
-		<ul>
-		  <li>Developed iPhone and iPad apps using Swift Playgrounds and SwiftUI, improving client workflows and driving satisfaction with tailored solutions.</li>
-		  <li>Designed and maintained databases, servers, and websites tailored to customer needs, ensuring seamless product integration and functionality.</li>
-		  <li>Collaborated closely with clients to identify needs, delivering tailored technical solutions that significantly improved user experience and satisfaction, directly contributing to product adoption and long-term retention.</li>
-		</ul>
-	  </div>
+      <div className="job">
+        <h3>Site Instructor at Girls Who Code</h3>
+        <span>Summer 2019 - 2021</span>
+        <ul>
+          <li>
+            Led an in-person classroom of over twenty aspiring computer
+            scientists, covering Python, HTML, CSS, GitHub, JavaScript & Arduino
+            C over seven weeks.
+          </li>
+          <li>
+            Developed core skills such as Agile development, debugging & the
+            importance of collaboration within a software team while offering
+            college preparedness.
+          </li>
+        </ul>
+      </div>
 
-	  <div className="job">
-		<h3>Client Support at Bloomberg LP</h3>
-		<span>2015 - 2016</span>
-		<ul>
-		  <li>Provided training and remote client support for PC and mobile software, handling troubleshooting inquiries and ensuring swift issue resolution.</li>
-		  <li>Drove the development of a technical support knowledge base, enhancing team efficiency and offering clients self-service resources that improved resolution times.</li>
-		</ul>
-	  </div>
+      <div className="job">
+        <h3>Software Developer at Peaking Software</h3>
+        <span>2016 - 2021</span>
+        <ul>
+          <li>
+            Learned to build iPhone apps using Swift Playgrounds. Embraced
+            Apple’s latest framework SwiftUI for building my latest iPhone &
+            iPad apps.
+          </li>
+          <li>
+            Developed databases, servers, websites and iPhone apps for clients.
+          </li>
+        </ul>
+      </div>
 
-	  <div className="job">
-		<h3>Genius Technician at Apple</h3>
-		<span>2013 - 2015</span>
-		<ul>
-		  <li>Resolved complex hardware and software issues with consistently high customer satisfaction. Contributed to an elevated NPS in a fast-paced, high-volume environment, driving loyalty and retention.</li>
-		  <li>Provided personalized One to One customer trainings, empowering users to fully leverage their Apple products, increasing product adoption, customer retention, and long-term loyalty. These tailored sessions consistently resulted in positive feedback and repeat customer engagement.</li>
-		</ul>
-	  </div>
+      <div className="job">
+        <h3>Client Support at Bloomberg LP</h3>
+        <span>2015 - 2016</span>
+        <ul>
+          <li>
+            Provided training & technical support for PC and mobile software,
+            coordinated field service visits, and performed troubleshooting.
+          </li>
+          <li>
+            Steered the initiative to create a wiki for the San Francisco
+            technical support team, ensuring clear, concise, and accurate
+            documentation of systems and procedures.
+          </li>
+        </ul>
+      </div>
 
-	  <div className="job">
-		<h3>In Store Guest Trainer at Apple</h3>
-		<span>2013</span>
-		<ul>
-		  <li>Trained new hires and staff on product knowledge, troubleshooting, and customer service, improving team performance and driving customer success.</li>
-		  <li>Developed and introduced mentorship programs that directly contributed to a significant increase in customer satisfaction, earning the Market Q2 Most Improved Net Promoter Score.</li>
-		</ul>
-	  </div>
+      <div className="job">
+        <h3>Genius Technician at Apple</h3>
+        <span>2013 - 2015</span>
+        <ul>
+          <li>
+            Supplied leadership with support including providing overrides for
+            the Redzone, relief for Leader on the Floor for breaks and conducted
+            One to One trainings.
+          </li>
+          <li>
+            Provided exceptional customer service and offered complete solutions
+            to customers, as reflected by my high NPS score as a technician in a
+            Training Store.
+          </li>
+        </ul>
+      </div>
 
-	  <div className="job">
-		<h3>Sales Specialist at Apple</h3>
-		<span>2011 - 2013</span>
-		<ul>
-		  <li>Spearheaded in-store workshops, directly contributing to an exceptional Net Promoter Score of 98 for three consecutive quarters by enhancing customer education and satisfaction through tailored product demonstrations.</li>
-		  <li>Collaborated with the Visual Merchandising Team during overnight shifts, ensuring products and displays were meticulously prepared to align with company guidelines, ultimately enhancing the customer experience through flawless presentation.</li>
-		</ul>
-	  </div>
-	</section>
-  );
+      <div className="job">
+        <h3>In Store Guest Trainer at Apple</h3>
+        <span>2013</span>
+        <ul>
+          <li>
+            Developed & facilitated training for employee on-boarding, ongoing
+            sales training, technical training, & train the trainer workshops as
+            In Store Guest Trainer.
+          </li>
+          <li>
+            Increased product & technical knowledge at the market level with my
+            employee mentorship initiative, which earned the market Q2 2013 Most
+            Improved Net Promoter Score.
+          </li>
+        </ul>
+      </div>
+
+      <div className="job">
+        <h3>Sales Specialist at Apple</h3>
+        <span>2011 - 2013</span>
+        <ul>
+          <li>
+            Led in store workshop team which earned the store Net Promoter Score
+            of 98 for three quarters.
+          </li>
+          <li>
+            Participated in overnight shifts as part of the Visual Merchandising
+            Team, preparing products and visuals, ensuring their presentation
+            adhered to exact measurements based on guidelines.
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
 }
--- components/Skills.js
+++ components/Skills.js
@@ -1,24 +1,34 @@
-import styles from './Card.module.css';
+import styles from './Card.module.css'
 
 export default function Skills() {
   return (
-	<section id="skills" className={styles.card}>
-	  <h2>Skills</h2>
-	  <h3>Soft Skills</h3>
-	  <ul>
-		<li>Exceptional communication and customer service skills, fostering strong relationships and delivering empathetic, customer-first support that drives customer success and satisfaction.</li>
-		<li>Proven ability to work effectively under pressure, manage tight deadlines, and coordinate cross-functional teams to meet customer needs.</li>
-		<li>Strong delegation and organizational skills, ensuring consistent service delivery with a customer-centric approach.</li>
-		<li>Meticulous attention to detail and a deep commitment to customer success and satisfaction.</li>
-	  </ul>
+    <section id="skills" className={styles.card}>
+      <h2>Skills</h2>
+      <h3>Soft Skills</h3>
+      <ul>
+        <li>
+          Strong communication, customer service skills, and empathy in
+          educational settings.
+        </li>
+        <li>
+          Ability to work under pressure, meet deadlines, and coordinate teams.
+        </li>
+        <li>Skilled in delegating tasks and delivering quality service.</li>
+        <li>Detail-oriented with a strong commitment to excellence.</li>
+      </ul>
 
-	  <h3>Technical Skills</h3>
-	  <ul>
-		<li>Expertise in troubleshooting hardware and software issues for Apple desktop and mobile devices, swiftly resolving technical problems to enhance the customer experience.</li>
-		<li>Proven ability to train and develop teams, empowering technical support and sales staff to deliver superior customer service and drive customer success by effectively addressing customer needs.</li>
-		<li>Experienced in providing remote technical support and training, ensuring customers can efficiently use products and services.</li>
-		<li>Proficient in mobile app and website development, enabling a comprehensive understanding of technical issues from a customer's perspective.</li>
-	  </ul>
-	</section>
-  );
+      <h3>Technical Skills</h3>
+      <ul>
+        <li>
+          Troubleshooting hardware and software issues for Apple desktop and
+          mobile computers.
+        </li>
+        <li>
+          Training and development for technical support teams and sales staff.
+        </li>
+        <li>Remote client technical support and training.</li>
+        <li>Proficient in mobile app and website development.</li>
+      </ul>
+    </section>
+  )
 }
