6f66d2804028076646190d9e3720601f4c4d6046
soliskit <doze.inept.0n@icloud.com>
2026-04-01 14:41:34 -0700

    update website year

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
-import styles from './Footer.module.css';
+import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
-	<footer className={styles.footer}>
-	  <p>&copy; 2025 David Solis. All rights reserved.</p>
-	</footer>
-  );
+    <footer className={styles.footer}>
+      <p>&copy; 2026 David Solis. All rights reserved.</p>
+    </footer>
+  )
 }
