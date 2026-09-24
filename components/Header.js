12cf20ea3e10acd18303649514005487602bf169
David Solis <doze.inept.0n@icloud.com>
2026-09-24 13:45:38 -0700

    Remove headline from header

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,6 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <h1>David Solis</h1>
-      <p className={styles.tagline}>
-        Fluent in Swift, English, and Spanish. English compiles now.
-      </p>
       <img
         src="/images/maya.png"
         alt="David Solis"
