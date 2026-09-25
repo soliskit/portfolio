6e86c771f87e1682a0afc8fa6dec128a5fecd933
David Solis <doze.inept.0n@icloud.com>
2026-09-24 20:22:05 -0700

    Fix degree name and punctuation

--- components/Education.js
+++ components/Education.js
@@ -4,8 +4,8 @@ export default function Education() {
   return (
     <section id="education" className={styles.card}>
       <h2>Education</h2>
-      <p>University of California, Riverside – Bachelors of Arts in History</p>
-      <p>Read, write and speak Spanish</p>
+      <p>University of California, Riverside – Bachelor of Arts in History</p>
+      <p>Read, write, and speak Spanish</p>
     </section>
   )
 }
